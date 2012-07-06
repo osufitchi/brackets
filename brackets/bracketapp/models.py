f5e8c894c9269a56f63d80c74fea692135a316d6
Ovidiu Sufitchi <osufitchi@gmail.com>
2012-07-06 09:58:39 -0400

    added models and registration view

--- brackets/bracketapp/models.py
+++ brackets/bracketapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.forms import ModelForm
+class Bracket(models.Model):
 
-# Create your models here.
+    maxnum_of_competitors  = models.IntegerField()
+    time = models.DateTimeField()
+    title = models.CharField(max_length=50)
+    
+    def __unicode__(self):
+        return self.title
+
+class Competitor(models.Model):
+
+    bracket = models.ManyToManyField(Bracket)
+    name = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.name
+
+class Competition(models.Model):
+
+    competitor_a = models.ForeignKey(Competitor)
+    competitor_b = models.ForeignKey(Competitor)
+    bracket = models.ForeignKey(Competitor)
+    
+    def __unicode__(self):
+        return u'%s vs %s' % (self.competitor_a, self.competitor_b)
--- brackets/bracketapp/views.py
+++ brackets/bracketapp/views.py
@@ -1,1 +1,32 @@
-# Create your views here.
+from bracket.models import Bracket, Competitor, Competition
+from django.shortcuts import redirect, render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+
+class RichUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(label = "First name")
+    last_name = forms.CharField(label = "Last name")
+    
+    def save(self, commit=True):
+        user = super(RichUserCreationForm, self).save(commit=False)
+        first_name =self.cleaned_data["first_name"]
+        last_name =self.cleaned_data["last_name"]
+        user.first_name = first_name
+        user.last_name = last_name
+        if commit:
+            user.save()
+        return user
+
+def register(request):
+    if request.method == 'POST':
+           form = RichUserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/login/")
+    else:
+            form = RichUserCreationForm()
+    return render_to_response("registration/register.html", {
+        'form': form,
+    },context_instance=RequestContext(request))
+
+
