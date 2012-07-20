8361dd44f72ba16800267c232f0f29fedfc033be
Ovidiu Sufitchi <osufitchi@gmail.com>
2012-07-20 10:56:50 -0400

    created css styling and modified base, matched url

--- brackets/urls.py
+++ brackets/urls.py
@@ -19,5 +19,5 @@ urlpatterns = patterns('',
      url(r'^admin/', include(admin.site.urls)),
      url(r'^newuser/$',register),
      url(r'^welcome/$',TemplateView.as_view(
-         template_name='registration/welcometohtml.html'))
+         template_name='base.html'))
 )
